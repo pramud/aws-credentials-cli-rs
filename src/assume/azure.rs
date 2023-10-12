f521beedf0359beecfb21995c9e308d4f6ce8faf
Rune Lausen <rune.lausen@lego.com>
2023-10-12 21:29:03 +0200

    Attempt Azure login after 1 unsuccessful attemt to get OIDC token.

--- src/assume/azure.rs
+++ src/assume/azure.rs
@@ -1,5 +1,3 @@
-use std::error::Error;
-
 use azure_core::auth::TokenCredential;
 use azure_identity::DefaultAzureCredential;
 
@@ -7,14 +5,26 @@ const AZ_CLIENT_ID: &str = "api://3cd4d944-d89b-401b-b2ae-fb1ece182362";
 const TOKEN_EXCHANGE_URL: &str = "https://ws-iam-commontools-oidc2saml.azurewebsites.net/api/TokenExchange/SAMLResponse";
 const IDENTIFIER_URI_BASE: &str = "https://signin.aws.amazon.com/saml/";
 
-pub async fn oidc_token() -> Result<String, Box<dyn Error>> {
+pub type Result<T> = std::result::Result<T, AzureAdTokenError>;
+
+#[derive(Debug, thiserror::Error)]
+pub enum AzureAdTokenError {
+    #[error(transparent)]
+    OidcTokenError(#[from] azure_core::Error),
+    #[error("Azure login failed")]
+    AzureLoginFailed,
+    #[error(transparent)]
+    SamlTokenError(#[from] reqwest::Error),
+}
+
+pub async fn oidc_token() -> Result<String> {
     let az_cli_credential = DefaultAzureCredential::default();
     let res = az_cli_credential.get_token(AZ_CLIENT_ID).await?;
     let token = res.token.secret();
     Ok(token.to_string())
 }
 
-pub async fn saml_token_from_oidc_token(account_id: &str, oidc_token: &str) -> Result<String, Box<dyn Error>> {
+pub async fn saml_token_from_oidc_token(account_id: &str, oidc_token: &str) -> Result<String> {
     let client = reqwest::Client::new();
     let mut identifier_uri = String::from(IDENTIFIER_URI_BASE);
     identifier_uri.push_str(account_id);
@@ -29,7 +39,22 @@ pub async fn saml_token_from_oidc_token(account_id: &str, oidc_token: &str) -> R
     Ok(saml_token)
 }
 
-pub async fn saml_token(account_id: &str) -> Result<String, Box<dyn Error>> {
-    let oidc_token = oidc_token().await?;
-    saml_token_from_oidc_token(account_id, &oidc_token).await
+pub async fn saml_token(account_id: &str) -> Result<String> {
+    match oidc_token().await {
+        Ok(oidc_token) => saml_token_from_oidc_token(account_id, oidc_token.as_str()).await,
+        Err(_error) => {
+            let successful_login = std::process::Command::new("az")
+                .arg("login")
+                .status()
+                .is_ok_and(|s| s.success());
+            if successful_login {
+                let oidc_token = oidc_token().await?;
+                saml_token_from_oidc_token(account_id, &oidc_token).await
+            } else {
+                log::error!("Azure login failed");
+                return Err(AzureAdTokenError::AzureLoginFailed);
+            }
+
+        }
+    }
 }
--- src/assume/mod.rs
+++ src/assume/mod.rs
@@ -5,13 +5,12 @@ mod aws;
 use std::error::Error;
 
 use aws::acquire_aws_credentials;
-use azure::saml_token;
 use models::TemporaryAwsCredentials;
 
 use crate::RoleInfo;
 
 pub async fn acquire_credentials(role_info: &RoleInfo) -> Result<TemporaryAwsCredentials, Box<dyn Error>> {
-    let saml_token = saml_token(&role_info.account_id).await?;
+    let saml_token = azure::saml_token(&role_info.account_id).await?;
     acquire_aws_credentials(role_info, &saml_token).await
 }
 
