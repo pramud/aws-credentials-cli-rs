fc41f60544350ea91f2211a9d802b901f891aca3
Rune Lausen <rune.lausen@lego.com>
2023-10-17 09:40:35 +0200

    Fix typo

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::process;
 #[tokio::main]
 async fn main() {
     if let Err(error) = aws_credentials_cli::run().await {
-        eprintln!("Applicaiton error: {error}");
+        eprintln!("Application error: {error}");
         process::exit(1);
     }
 }
